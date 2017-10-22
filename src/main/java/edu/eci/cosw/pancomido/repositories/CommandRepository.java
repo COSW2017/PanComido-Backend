04dd271fe4f0b505cc1dbd42f0d96e8e0795bd8d
marialebu <mabu12@outlook.com>
2017-10-22 17:10:35 -0500

    Arregladas las consultas

--- src/main/java/edu/eci/cosw/pancomido/repositories/CommandRepository.java
+++ src/main/java/edu/eci/cosw/pancomido/repositories/CommandRepository.java
@@ -10,15 +10,15 @@ import java.util.List;
 public interface CommandRepository extends JpaRepository<Command, Integer> {
 
     @Query("SELECT c FROM Command as c WHERE c.id_order = ?1")
-    public List<Command> getCommandsByOrder(Integer orderId);
+    List<Command> getCommandsByOrder(Integer orderId);
 
     //No sé si este bien supongo que no...
-    @Query("select cd.id_command from Command_Dish as cd inner join cd.id_dish as d where d.restaurant.id_restaurant = ?1")
-    public List<Command> getCommandByRestaurant(Integer restaurantId);
+    @Query("select cd.id_command from Command_Dish as cd inner join cd.id_dish as d where d.restaurant.id_restaurant = ?1 group by cd.id_command")
+    List<Command> getCommandByRestaurant(Integer restaurantId);
 
     //Falta probar
     @Query("select c from Command as c where c.id_command = ?1")
-    public Command getCommand(Integer CommandId);
+    Command getCommand(Integer CommandId);
 
     @Query("select cd.id_dish from Command_Dish as cd inner join cd.id_command as c where c.id_command = ?1")
     List<Dish> getDishes(Integer id_command);
