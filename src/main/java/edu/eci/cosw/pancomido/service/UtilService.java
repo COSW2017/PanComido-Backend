358639b845fcfd3ac262212ae25ec7ca888497bb
Nicolas Gomez Solano <nicolasgomezsolano@yahoo.es>
2017-12-10 23:59:43 -0500

    Push notifications test

--- src/main/java/edu/eci/cosw/pancomido/service/UtilService.java
+++ src/main/java/edu/eci/cosw/pancomido/service/UtilService.java
@@ -3,7 +3,9 @@ package edu.eci.cosw.pancomido.service;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -15,7 +17,7 @@ public class UtilService {
     private static String authKey = "AAAAKpzergM:APA91bHSCZbfp7LyScDPl25l49f8WoV_O6FFWwLMn-W0sAtk4R7ksVBH4wk26UjMCujZKJUyNRiiHzsIl7jTY7lkIaLajH2XuALKoKIYXCc19ToaM6D0mtV-R72Px2vCMqqT3MMgfgZJ";
     private static String FMCurl = "http://fcm.googleapis.com/fcm/send";
 
-    public static void sendNotifications(String u, String message, String title){
+    /*public static void sendNotifications(String u, String message, String title){
         String real_email = u.replaceAll("\\.","dot").replaceAll("@","at");
         URL url = null;
         try {
@@ -58,6 +60,54 @@ public class UtilService {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+    }*/
+
+    public static String sendNotifications(String u, String message, String title)
+            throws IOException {
+        String real_email = u.replaceAll("\\.","dot").replaceAll("@","at");
+        String result = "";
+        URL url = new URL(FMCurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setUseCaches(false);
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", "key=" + authKey);
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        JSONObject json = new JSONObject();
+
+        json.put("to","/topics/user~"+real_email );
+        JSONObject info = new JSONObject();
+        info.put("title", "notification title"); // Notification title
+        info.put("body", "message body"); // Notification
+        // body
+        json.put("notification", info);
+        try {
+            OutputStreamWriter wr = new OutputStreamWriter(
+                    conn.getOutputStream());
+            wr.write(json.toString());
+            wr.flush();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+            result = "SUCCESS";
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "BAD";
+        }
+        System.out.println("GCM Notification is sent successfully");
+
+        return result;
 
     }
 }
